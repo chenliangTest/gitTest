6ba140059f53262d0ecbe10a81eb40814d8f1e5e
chenliangTest <cheng3417885@163.com>
2016-12-08 17:30:21 +0800

    test
    Signed-off-by: chenliangTest <cheng3417885@163.com>

---
+++ src/com/test/__init__.py
@@ -0,0 +1,16 @@
+print ("hello world!")
+print (2 + 2)
+print (1.0 / 2)
+print (1 // 2)
+print (10)
+#x=raw_input("x:" )
+#print x
+A=['name', 'chen']
+print (A[0])
+B=['name', 'liang']
+database=[A ,B]
+print (database)
+print (database[0])
+tag='<a href="http://www.baidu.com"></a>'
+print (tag[9:29])
+
